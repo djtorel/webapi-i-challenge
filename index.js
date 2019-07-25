40fc989e934271457f9ae4ec13937029a7139aac
Dominic Torruellas <dom@domtorr.com>
2019-07-24 20:01:53 -0700

    GET - /api/users/:id - Get user by ID

--- index.js
+++ index.js
@@ -27,4 +27,16 @@ server.post('/api/users', (req, res) => {
     });
 });
 
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+});
+
 server.listen(8000, console.log('API running on port 8000'));
