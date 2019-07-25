6dafa7ef08f389c45e0807b62252ff221432b99f
Dominic Torruellas <dom@domtorr.com>
2019-07-24 19:58:26 -0700

    POST - /api/users - Add a user

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const db = require('./data/db');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/api/users', (req, res) => {
   db.find()
     .then(users => {
       res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+  const userInfo = req.body;
+
+  db.insert(userInfo)
+    .then(user => {
+      res.status(201).json({ success: true, user });
     })
     .catch(err => {
       res.status(500).json({ success: false, err });
