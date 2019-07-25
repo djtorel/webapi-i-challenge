0cbb497ca7e8ebe60ab1a9b88a2603a0eac92db2
Dominic Torruellas <dom@domtorr.com>
2019-07-24 20:26:58 -0700

    Added  requirements for GET users by Id

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ server.post('/api/users', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-          error: 'There was an error while saving the user to the database',
-        });
+        error: 'There was an error while saving the user to the database',
+      });
     });
 });
 
@@ -43,10 +43,17 @@ server.get('/api/users/:id', (req, res) => {
 
   db.findById(id)
     .then(user => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
       res.status(200).json(user);
     })
     .catch(err => {
-      res.status(500).json({ success: false, err });
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
     });
 });
 
